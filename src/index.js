d4b0de2c97d891bc1b6f8db8595811ee7d389c28
Talal Arshad <talal7860@gmail.com>
2023-06-12 13:29:06 -0600

    build

--- src/index.js
+++ src/index.js
@@ -60,8 +60,8 @@ const gqlReq = ({query, variables}) => new Promise((resolve, reject) => {
     let data = '';
     res.on('data', (d) => data += d);
     res.on('end', () => {
-      core.debug('Response', data)
-      core.debug('Status', res.statusCode)
+      core.debug(`Response: ${data}`)
+      core.debug(`Status ${res.statusCode}`)
       const json = JSON.parse(data)
       if (json.errors) {
         reject(json.errors)
@@ -80,7 +80,7 @@ gqlReq({query: prQuery, variables : {
   owner,
   name: repo,
 }}).then((prs) => {
-  core.debug('PRs', JSON.stringify(prs))
+  core.debug(`PRs ${JSON.stringify(prs)}`)
   prs.data.repository.pullRequests.nodes.filter((pr) => {
     const updatedAt = new Date(pr.updatedAt)
     const now = new Date()
